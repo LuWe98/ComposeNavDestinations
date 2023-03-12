8979fce5153011c3109d99005e38732f65fed9e7
lucla <luca.we98@gmx.de>
2023-03-12 16:55:36 +0100

    Fixed NavBackStackEntry Extension

--- compose-nav-destinations/src/main/java/com/welu/composenavdestinations/extensions/navigation/NavBackStackEntryExt.kt
+++ compose-nav-destinations/src/main/java/com/welu/composenavdestinations/extensions/navigation/NavBackStackEntryExt.kt
@@ -2,10 +2,14 @@ package com.welu.composenavdestinations.extensions.navigation
 
 import androidx.navigation.NavBackStackEntry
 import com.welu.composenavdestinations.navigation.destinations.ComposeDestination
+import com.welu.composenavdestinations.navigation.spec.ComposeDestinationSpec
 import com.welu.composenavdestinations.service.ServiceLocator
 
 @Suppress("UNCHECKED_CAST", "DEPRECATION")
 internal operator fun <T> NavBackStackEntry.get(key: String): T? = arguments?.get(key) as T?
 
 val NavBackStackEntry.navComponentSpec get() = ServiceLocator.composeDestinationService.findComponentSpec(destination.route!!)
-val NavBackStackEntry.composeDestination get() = navComponentSpec as ComposeDestination<*>
+
+val NavBackStackEntry.composeDestinationSpec get() = navComponentSpec as ComposeDestinationSpec<*>
+
+val NavBackStackEntry.composeDestination get() = composeDestinationSpec.destination
